1156601ed7d771bcf68ef0e6cb65cd868d7e8c22
lab661 <lab661@PC-446.urel.feec.vutbr.cz>
2019-12-11 12:30:04 +0100

    Device reads from BME sensor and displays on Nokia

--- projects/test-BMP/main.c
+++ projects/test-BMP/main.c
@@ -1,14 +1,13 @@
 /*
  * ---------------------------------------------------------------------
- * Author:      Tomas Fryza
+ * Author:      Hana Stolarova,  Vladimir Lahoda
  *              Dept. of Radio Electronics, Brno Univ. of Technology
- * Created:     2017-10-27
- * Last update: 2019-11-08
+ * Created:     2019-11-26
  * Platform:    ATmega328P, 16 MHz, AVR 8-bit Toolchain 3.6.2
  * ---------------------------------------------------------------------
  * Description:
- *    Scan TWI bus for all connected slave devices and transmit info
- *    to UART.
+ *   Project for our seminar DE2. It messures hight (altitude) based on pressure data and 
+ *   compares it to GPS data.
  */
 
 /* Includes ----------------------------------------------------------*/
@@ -34,38 +33,33 @@
 
 
 /* Functions ---------------------------------------------------------*/
-/**
- *  Brief:  Main program. Test all TWI slave addresses and scan 
- *          connected devices.
- *  Input:  None
- *  Return: None
- */
 
-int calc_alt(double pressure, double press_ref)
+
+int calc_alt(double pressure, double press_ref) //Function for calc altitude from BME pressure
 {
     double cit = log10(pressure/press_ref) * press_ref;
     double jmen = log10(2.718281) * 1.225 * 9.98;
     double alt = -cit / jmen;
     return alt;
 }
-void display_values(int pressure, int altitude_b, int altitude_gps, int temperature)
+void display_values(int pressure, int altitude_b, int altitude_gps, int temperature) //Function fordisplay values 
  {
     int altitude_dif = altitude_gps - altitude_b;
 
-    char pressure_ch[10];
+    char pressure_ch[10];           // declaration of char fields so they can be displayed on Nokia display
     char altitude_b_ch[10];
     char altitude_gps_ch[10];
     char altitude_dif_ch[10];
     char temperature_ch[10];
 
-    itoa(pressure, pressure_ch, 10);
+    itoa(pressure, pressure_ch, 10);           // conversion from int data to string   
     itoa(altitude_b, altitude_b_ch, 10);
     itoa(altitude_gps, altitude_gps_ch, 10);
     itoa(altitude_dif, altitude_dif_ch, 10);
     itoa(temperature, temperature_ch, 10);
     
-    nokia_lcd_clear();
-    nokia_lcd_write_string("Pres:",1);
+    nokia_lcd_clear();                          //clear display
+    nokia_lcd_write_string("Pres:",1);          //display values of char fields - pressure, altitude, temperature, GPS coordinates
     nokia_lcd_write_string(pressure_ch,1);
     nokia_lcd_set_cursor(0, 10);
     nokia_lcd_write_string("Alt BMP:", 1);
@@ -81,7 +75,13 @@ void display_values(int pressure, int altitude_b, int altitude_gps, int temperat
     nokia_lcd_write_string(temperature_ch,1);
     nokia_lcd_render();
  }
-void uart_print(char *name, long val)
+ 
+/*
+*               function we are not using in our project, 
+*               but when we tried to make the project work from the beginning,
+*               it wasnt working properly without this particular function 
+*               now the project is working as it should without this function, but just in case we left it here
+void uart_print(char *name, long val)   
 {
 	char debug_buffer[UART_BUFLEN];
 
@@ -91,30 +91,25 @@ void uart_print(char *name, long val)
         ltoa((val), debug_buffer, UART_BUFLEN);
         uart_puts(debug_buffer);
         uart_puts("\n");
-}
+}*/
+
 int main(void)
 {
     // UART: asynchronous, 8-bit data, no parity, 1-bit stop
     uart_init(UART_BAUD_SELECT(UART_BAUD_RATE, F_CPU));
     uart_puts("ok");
-    // TWI
+    // Initialisation BME 280
     bmp280_init();
     uart_puts("ok");
-    /* Timer1
-     * TODO: Configure Timer1 clock source and enable overflow 
-     *       interrupt */
     
+    // Timer1
     TIM_config_prescaler(TIM1, TIM_PRESC_256);
     TIM_config_interrupt(TIM1, TIM_OVERFLOW_ENABLE);
-   
+    // Display init
     nokia_lcd_init();
     nokia_lcd_clear();
     // Enables interrupts by setting the global interrupt mask
     sei();
-
-    // Put strings to ringbuffer for transmitting via UART.
-
-
 
     // Infinite loop
     for (;;) {
@@ -125,15 +120,14 @@ int main(void)
     return (0);
 }
 
-/**
- *  Brief: Timer1 overflow interrupt routine. Update state of TWI Finite
- *         State Machine.
- */
+
+    // Timer1 overflow interrupt routine.
+ 
 ISR(TIMER1_OVF_vect)
 {
     
-    double press_ref = 1013;
-	bmp280_measure();
+    double press_ref = 1013;        //Select pressure reference - we want to add a function which will display a choice of pressure reference
+	bmp280_measure();               // Request for download actual data from BME registers
 	double	pressure = (bmp280_getpressure()/100+32);
     double altitude_b = calc_alt(pressure, press_ref);
     int altitude_gps = 225;
